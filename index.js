de57d38d4380fd089178e8679a3f81463c8ed4ef
Borys Palka <borys.palka@codete.com>
2020-08-03 02:10:27 +0200

    add all complited tasks

--- index.js
+++ index.js
@@ -5,10 +5,89 @@
  */
 exports.example = () => 'hello world';
 
-exports.stripPrivateProperties = () => {};
-exports.excludeByProperty = () => {};
-exports.sumDeep = () => {};
-exports.applyStatusColor = () => {};
-exports.createGreeting = () => {};
-exports.setDefaults = () => {};
-exports.fetchUserByNameAndUsersCompany = () => {};
+exports.stripPrivateProperties = (filterProporties = [], arrayObj = []) => {
+    if (!filterProporties || filterProporties.length < 1) {
+        return arrayObj;
+    };
+    return arrayObj.map(obj => {
+        filterProporties.forEach(filter => delete obj[filter]);
+        return obj;
+    });
+}
+exports.excludeByProperty = (property, arrayObjs = []) => {
+    if (!property) {
+        return arrayObjs;
+    };
+    return arrayObjs.filter(obj => !Object.keys(obj).includes(property));
+};
+exports.sumDeep = (objects = []) => {
+    if (!objects) {
+        return 0;
+    }
+    return objects.map(({ objects }) =>
+        ({
+            objects: objects.reduce((prev, { val }) => prev + val, 0)
+        })
+    )
+};
+
+/**
+ * Map statusColor Object to status color
+ * @param statusColors e.g. {red:[101,202]}
+ * @returns {Object} e.g {101:'red',202:'red'}
+ */
+const mapStatusColor = (statusColors = {}) => {
+    const mappedStatusColor = {};
+    Object.entries(statusColors).forEach(([color, statuses]) =>
+        statuses.forEach(status => {
+            mappedStatusColor[status] = !mappedStatusColor[status] ? color : mappedStatusColor[status]
+        })
+    );
+    return mappedStatusColor;
+}
+
+exports.applyStatusColor = (statusColors = {}, statuses = []) => {
+    const mappedStatusToColor = mapStatusColor(statusColors);
+    return statuses.filter(({ status }) => mappedStatusToColor[status])
+        .map(({ status }) =>
+            ({
+                status,
+                color: mappedStatusToColor[status]
+            })
+        )
+};
+
+exports.createGreeting = (greetFunc, greetingString = "") =>
+    (name) => greetFunc(greetingString, name);
+
+exports.setDefaults = (defaultProporties = {}) => (obj = {}) =>
+    ({
+        ...defaultProporties,
+        ...obj
+    });
+
+exports.fetchUserByNameAndUsersCompany = (userName = "", { fetchStatus, fetchUsers, fetchCompanyById }) => {
+    if (!userName) {
+        return;
+    }
+    const getUserByName = (users) => users.find(({ name }) => name === userName);
+
+    return Promise.all([
+        fetchStatus(),
+        fetchUsers(),
+    ]).then(async ([status, users]) => {
+        const user = getUserByName(users)
+        const company = await fetchCompanyById(user.companyId)
+        return ({
+            status,
+            user,
+            company
+        })
+    }).catch(
+        e => console.error(e)
+    );
+};
+
+
+
+
